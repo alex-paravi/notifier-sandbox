7600743640ddcde28d39f06fa47e8370e23b63ac
alexparavi <alexparavi@gmail.com>
2026-06-03 17:30:22 +0400

    fix: update placeholder text

--- src/Senders/EmailSender.php
+++ src/Senders/EmailSender.php
@@ -9,6 +9,6 @@ class EmailSender implements NotificationSenderInterface
     #[Override]
     public function send(string $to, string $message): bool
     {
-        return "Робот отправил СМС на номер {$to}: {$message}\n";
+        return "Робот отправил сообщение на почту {$to}: {$message}\n";
     }
 }
