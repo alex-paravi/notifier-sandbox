2497f721433be9246aeb09d260ad8b0e2ee600b8
alexparavi <alexparavi@gmail.com>
2026-06-03 17:56:44 +0400

    feat: implement notification strategies and test processor

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 
+use Apara\NotifierSandbox\Senders\EmailSender;
+use Apara\NotifierSandbox\Senders\SmsSender;
+use Apara\NotifierSandbox\Senders\TelegramSender;
+use Apara\NotifierSandbox\SendProcessor;
+
 require_once __DIR__ . '/vendor/autoload.php';
+
+
+$emailSender = new EmailSender;
+$smsSender = new SmsSender;
+$telegramSender = new TelegramSender;
+
+$processor = new SendProcessor($emailSender);
+
+$processor->runProcess("test@mail.test", "test message");
+
+$processor->setSender($smsSender);
+
+$processor->runProcess("+2123213", "test message");
+
+$processor->setSender($telegramSender);
+
+$processor->runProcess("+2123213", "test message");
